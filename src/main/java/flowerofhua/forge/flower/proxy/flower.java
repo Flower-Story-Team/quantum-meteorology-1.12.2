573c4041e1075117beab727947e9c220e7499854
konpi <chenangelo003@gmail.com>
2020-07-10 14:35:48 +0200

    base

---
+++ src/main/java/flowerofhua/forge/flower/proxy/flower.java
@@ -0,0 +1,41 @@
+package flowerofhua.forge.flower.proxy;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * @author ustc_zzzz
+ */
+@Mod(modid = flower.MODID, name = flower.NAME, version = flower.VERSION, acceptedMinecraftVersions = "1.8.9")
+public class flower
+{
+    public static final String MODID = "fmltutor";
+    public static final String NAME = "FML Tutor";
+    public static final String VERSION = "1.0.0";
+
+    @Instance(flower.MODID)
+    public static flower instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        // TODO
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // TODO
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        // TODO
+    }
+}
