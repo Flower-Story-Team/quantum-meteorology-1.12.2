27ab48c31ed3ceb48c7cf3ac7569abd119870be2
konpi <chenangelo003@gmail.com>
2020-07-10 14:44:05 +0200

    base

--- src/main/java/flowerofhua/forge/flower/proxy/flower.java
+++ src/main/java/flowerofhua/forge/flower/proxy/flower.java
@@ -14,8 +14,8 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = flower.MODID, name = flower.NAME, version = flower.VERSION, acceptedMinecraftVersions = "1.12.2")
 public class flower
 {
-    public static final String MODID = "fmltutor";
-    public static final String NAME = "FML Tutor";
+    public static final String MODID = "flower";
+    public static final String NAME = "Flower Of Hua";
     public static final String VERSION = "1.0.0";
 
     @Instance(flower.MODID)
