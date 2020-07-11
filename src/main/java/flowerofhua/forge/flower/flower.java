a1546966b795b81a674ec260c58ed01b0be7b17d
Beanflame <2962672241@qq.com>
2020-07-11 08:23:32 +0800

    修改mod版本

--- src/main/java/flowerofhua/forge/flower/flower.java
+++ src/main/java/flowerofhua/forge/flower/flower.java
@@ -2,7 +2,6 @@ package flowerofhua.forge.flower;
 
 import flowerofhua.forge.flower.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -19,12 +18,13 @@ public class flower
 {
     public static final String MODID = "flower";
     public static final String NAME = "Flower Of Hua";
-    public static final String VERSION = "1.0.  0";
+    public static final String VERSION = "1.0.0";
 
     private Logger logger = LogManager.getLogger(flower.NAME);
 
 
-    public Logger getLogger() {
+    public Logger getLogger()
+    {
         return logger;
     }
     @SidedProxy(clientSide = "flowerofhua.forge.flower.proxy.ClientProxy",
