8e4b2670b3822a376d7ce18b27b2182cf75c4d8f
tangguoQAQ <64019266+tangguoQAQ@users.noreply.github.com>
2020-07-12 15:48:51 +0800

    Update ClientProxy.java
    简化代码

--- src/main/java/com/konpi/flower/proxy/ClientProxy.java
+++ src/main/java/com/konpi/flower/proxy/ClientProxy.java
@@ -1,8 +1,6 @@
 package com.konpi.flower.proxy;
 
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.*;
 
 public class ClientProxy extends CommonProxy
 {
