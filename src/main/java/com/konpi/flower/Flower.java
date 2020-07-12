0620eaa8e01113097c8772834b8a33b66e61701c
bf233 <3212517216@qq.com>
2020-07-12 18:23:35 +0800

    add lop

--- src/main/java/com/konpi/flower/Flower.java
+++ src/main/java/com/konpi/flower/Flower.java
@@ -15,7 +15,7 @@ public class Flower
 	public static final String MODID = "flower";
     public static final String NAME = "Flower of Hua";
     public static final String VERSION = "1.0.0";
-    
+    //1
     public static Logger logger = LogManager.getLogger(Flower.NAME);
     
     @SidedProxy(clientSide = "com.konpi.flower.proxy.ClientProxy", serverSide = "com.konpi.flower.proxy.CommonProxy")
