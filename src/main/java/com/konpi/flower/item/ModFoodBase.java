1c77ec2db9ce990234db88fba0daa576634a304d
tangguoQAQ <64019266+tangguoQAQ@users.noreply.github.com>
2020-07-12 19:09:21 +0800

    添加注解

--- src/main/java/com/konpi/flower/item/ModFoodBase.java
+++ src/main/java/com/konpi/flower/item/ModFoodBase.java
@@ -7,13 +7,14 @@ import net.minecraft.item.ItemFood;
 /**
  * 食物的基础类
  */
-public class ModFoodBase extends ItemFood
+
+//有参数构建函数
+public ModFoodBase(String registryName, int amount, float saturation, boolean isWolfFood)
+//ModFoodBase([字符串型,物品注册名(ID)],[整数型，回复的饥饿值(2点=一个鸡腿)],[小数型,添加的饱和度(用于回血)],[布尔值,能否给驯服的狼吃])
 {
-    public ModFoodBase(String registryName, int amount, float saturation, boolean isWolfFood)
-    {
-        super(amount, saturation, isWolfFood);
-        this.setRegistryName(registryName);
-        this.setTranslationKey(Flower.MODID + "." + registryName);
-        this.setCreativeTab(FlowerCreativeTabs.FOOD);
-    }
+    super(amount, saturation, isWolfFood);
+    this.setRegistryName(registryName);//注册名
+    this.setTranslationKey(Flower.MODID + "." + registryName);//lang文件相关
+    this.setCreativeTab(FlowerCreativeTabs.FOOD);//添加到物品栏
 }
+
