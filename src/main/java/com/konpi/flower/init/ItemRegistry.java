3ce22cdc830b864785b9f91388a3c19fbd096621
Bean_Flame <beanflame@163.com>
2020-07-12 18:51:24 +0800

    修改 ItemRegistry类

--- src/main/java/com/konpi/flower/init/ItemRegistry.java
+++ src/main/java/com/konpi/flower/init/ItemRegistry.java
@@ -24,16 +24,15 @@ public class ItemRegistry
         //向一些固定类型的集合里添加一些东西
         Flower.logger.info("initializing normal items");
         
+        
         simpleItemList.add(new ModFoodBase("rice_seed", 1, 1, false));	//水稻种子
-        
         simpleItemList.add(new ModFoodBase("rice", 1, 1, false));	//大米
-        
         simpleItemList.add(new ModFoodBase("bowl", 1, 1, false));	//碗
+        
     }
-
-    /**
-     * 注册Item
-     */
+    
+    
+    /**注册Item*/
     @SubscribeEvent
     public static void onItemRegister(RegistryEvent.Register<Item> event)
     {
@@ -46,28 +45,24 @@ public class ItemRegistry
 
         //注册普通物品
         Flower.logger.info("registering items");
+        
+        
         event.getRegistry().registerAll(simpleItemList.toArray(new Item[0]));	//水稻种子
-        
         event.getRegistry().registerAll(simpleItemList.toArray(new Item[1]));	//大米
-        
         event.getRegistry().registerAll(simpleItemList.toArray(new Item[2]));	//碗
 
     }
     
     
+    /**注册物品后这个会自动变成对应物品的引用*/
     @ObjectHolder(Flower.MODID)
-    
     public static class ItemHolder
     {
-    	//注册物品后这个会自动变成对应物品的引用
+    	
     	@ObjectHolder("rice_seed")	//水稻种子
         public static final Item RICE_SEED = null;	//水稻种子
-    	
-
-        
         @ObjectHolder("rice")
         public static final Item RICE = null;
-        
         @ObjectHolder("bowl")	//碗
         public static final Item BOWL = null;	//碗
 
