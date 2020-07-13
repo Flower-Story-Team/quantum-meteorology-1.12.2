e9b49df7ce42318b2a792fca905cdd71c6e4a82f
Bean_Flame <beanflame@163.com>
2020-07-13 11:46:19 +0800

    添加 番茄

--- src/main/java/com/konpi/flower/init/ItemRegistry.java
+++ src/main/java/com/konpi/flower/init/ItemRegistry.java
@@ -18,27 +18,33 @@ public class ItemRegistry
 	//装普通物品的容器
     public static final ArrayList<Item> simpleItemList = new ArrayList<>();
 
-    //在这里往容器里添加物品，加进去就自动注册了
+    //在这里往容器里添加物品, 加进去就自动注册了
     static
     {
         //实例化普通物品
     	Flower.logger.info("initializing normal items");
         
+    	
     	//种子
         simpleItemList.add(new ModItemBase("rice_seed").setCreativeTab(ModCreativeTabs.MISC)); //水稻种子
 
+        
         //食材
     	simpleItemList.add(new ModItemBase("rice").setCreativeTab(ModCreativeTabs.MISC)); //生米
         simpleItemList.add(new ModFoodBase("pepper_chili", 1, 0.6F, false)); //辣椒
         simpleItemList.add(new ModFoodBase("cabbage", 1, 0.6F, false)); //白菜
+        simpleItemList.add(new ModFoodBase("tomato", 1, 0.6F, false)); //番茄
         simpleItemList.add(new ModFoodBase("corn", 1, 0.6F, false)); //玉米
 
+        
         //熟食
         simpleItemList.add(new ItemFoodInBowl("steamed_rice", 1, 0.6F, false)); //米饭
     	
+        
     	//餐具
         simpleItemList.add(new ModTablewareBase("bowl").setMaxStackSize(16)); //碗
 
+        
         //实例化方块物品
         Flower.logger.info("initializing block items");
         for (Block b : BlockRegistry.simpleBlockList)
@@ -76,6 +82,18 @@ public class ItemRegistry
 
         @ObjectHolder("cabbage")	//白菜
         public static final Item CABBAGE = null;
+        
+        @ObjectHolder("tomato")		//番茄
+        public static final Item TOMATO = null;
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
         @ObjectHolder("corn")	//玉米
         public static final Item CORN = null;
