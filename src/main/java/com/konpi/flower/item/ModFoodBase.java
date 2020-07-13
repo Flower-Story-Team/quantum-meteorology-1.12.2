5ea559d3f65e2d1444ae819c78ecc1a8e27927d3
tangguoQAQ <64019266+tangguoQAQ@users.noreply.github.com>
2020-07-13 08:43:37 +0800

    添加食物堆数量

--- src/main/java/com/konpi/flower/item/ModFoodBase.java
+++ src/main/java/com/konpi/flower/item/ModFoodBase.java
@@ -10,10 +10,11 @@ import net.minecraft.item.ItemFood;
 public class ModFoodBase extends ItemFood
 {
 	//有参数构建函数
-	/**ModFoodBase([字符串型,物品注册名(ID)],[整数型，回复的饥饿值(2点=一个鸡腿)],[小数型,添加的饱和度(用于回血)],[布尔值,能否给驯服的狼吃]) */
-    public ModFoodBase(String registryName, int amount, float saturation, boolean isWolfFood)
+	/**ModFoodBase([字符串型,物品注册名(ID)],[整数型，回复的饥饿值(2点=一个鸡腿)],[小数型,添加的饱和度(用于回血)],[布尔值,能否给驯服的狼吃]),[整数型,物品最大堆叠数量])*/
+    public ModFoodBase(String registryName, int amount, float saturation, boolean isWolfFood, int MaxStackSize)
     {
         super(amount, saturation, isWolfFood);
+        this.setMaxStackSize(MaxStackSize);
         this.setRegistryName(registryName);
         this.setTranslationKey(Flower.MODID + "." + registryName);
         this.setCreativeTab(FlowerCreativeTabs.FOOD);
