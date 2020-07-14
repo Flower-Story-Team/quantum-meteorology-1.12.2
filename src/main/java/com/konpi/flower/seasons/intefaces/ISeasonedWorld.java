1c403847f7a4e0f2522e0faafe104a2405692ba9
ylll <68209038+yllllyb@users.noreply.github.com>
2020-07-15 00:15:57 +0800

    Create ISeasonedWorld.java

---
+++ src/main/java/com/konpi/flower/seasons/intefaces/ISeasonedWorld.java
@@ -0,0 +1,15 @@
+package com.konpi.flower.seasons.intefaces;
+
+import com.konpi.flower.seasons.Season;
+
+import net.minecraft.util.math.BlockPos;
+
+/**
+ * according to sereneseason
+ *
+ */
+public interface ISeasonedWorld {
+	boolean canSnowAtInSeason(BlockPos pos, boolean checkLight, Season season);
+
+	boolean canBlockFreezeInSeason(BlockPos pos, boolean noWaterAdj, Season season);
+}
