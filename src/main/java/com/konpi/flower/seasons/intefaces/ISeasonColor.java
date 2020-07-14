9a545f590950380f1a24e9df286653a1c9c65f42
ylll <68209038+yllllyb@users.noreply.github.com>
2020-07-15 00:12:42 +0800

    Update ISeasonColor.java

--- src/main/java/com/konpi/flower/seasons/intefaces/ISeasonColor.java
+++ src/main/java/com/konpi/flower/seasons/intefaces/ISeasonColor.java
@@ -1,10 +1,18 @@
 
 package com.konpi.flower.seasons.intefaces;
 
+/**
+ * according to sereneseason
+ *
+ */
 public interface ISeasonColor {
-    int getGrassColor();
-    float getGrassSaturation();
-    int getFoliageColor();
-    float getFoliageSaturation();
-}
+	int getGrassColor();
 
+	float getGrassSaturation();
+
+	int getFoliageColor();
+
+	float getFoliageSaturation();
+
+	int getBirchColor();
+}
