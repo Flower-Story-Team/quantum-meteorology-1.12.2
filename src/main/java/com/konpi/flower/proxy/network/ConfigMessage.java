8397e3f1a5b33b4f39cf26006066a8496380b8dc
ylll <68209038+yllllyb@users.noreply.github.com>
2020-07-15 13:20:01 +0800

    Create ConfigMessage.java

---
+++ src/main/java/com/konpi/flower/proxy/network/ConfigMessage.java
@@ -0,0 +1,51 @@
+package com.konpi.flower.proxy.network;
+
+import com.konpi.flower.Flower;
+import com.konpi.flower.api.config.SyncedConfig;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
+
+public class ConfigMessage implements IMessage, IMessageHandler<ConfigMessage, IMessage> {
+
+	public NBTTagCompound nbtOptions;
+
+	public ConfigMessage() {
+	}
+
+	public ConfigMessage(NBTTagCompound nbtOptions) {
+		this.nbtOptions = nbtOptions;
+	}
+
+	@Override
+	public void fromBytes(ByteBuf buf) {
+		this.nbtOptions = ByteBufUtils.readTag(buf);
+	}
+
+	@Override
+	public void toBytes(ByteBuf buf) {
+		ByteBufUtils.writeTag(buf, nbtOptions);
+	}
+
+	@Override
+	public IMessage onMessage(ConfigMessage message, MessageContext ctx) {
+		if (ctx.side == Side.CLIENT) {
+			for (String key : message.nbtOptions.getKeySet()) {
+				SyncedConfig.SyncedConfigEntry entry = SyncedConfig.optionsToSync.get(key);
+
+				if (entry == null)
+					Flower.logger.error("Option " + key + " does not exist locally!");
+
+				entry.value = message.nbtOptions.getString(key);
+				Flower.logger.info("Flower configuration synchronized with the server");
+			}
+		}
+
+		return null;
+	}
+}
