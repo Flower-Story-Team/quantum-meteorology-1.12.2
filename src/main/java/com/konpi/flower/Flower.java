3454ccfe33680d204c7f673b4eb5dc6ff0775748
Bean_Flame <beanflame@163.com>
2020-07-20 16:07:03 +0800

    修改 MODID

--- src/main/java/com/konpi/flower/Flower.java
+++ src/main/java/com/konpi/flower/Flower.java
@@ -23,10 +23,10 @@ import org.apache.logging.log4j.Logger;
  */
 @Mod(modid = Flower.MODID, name = Flower.NAME, version = Flower.VERSION, acceptedMinecraftVersions = "1.12.2")
 public class Flower {
-	public static final String MODID = "flower";
-	public static final String NAME = "Flower of Hua";
+	public static final String MODID = "quantummeteorology";
+	public static final String NAME = "Quantum Meteorology";
 	public static final String VERSION = "1.0.1";
-
+	
 	public static Logger logger = LogManager.getLogger(Flower.NAME);
 
 	@SidedProxy(clientSide = "com.konpi.flower.proxy.ClientProxy", serverSide = "com.konpi.flower.proxy.CommonProxy")
