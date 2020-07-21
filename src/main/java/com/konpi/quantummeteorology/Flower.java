6f3acee0d138bd55643b34b481c6bb5968cb23b0
Bean_Flame <beanflame@163.com>
2020-07-21 09:16:35 +0800

    更新 文件

--- src/main/java/com/konpi/quantummeteorology/Flower.java
+++ src/main/java/com/konpi/quantummeteorology/Flower.java
@@ -2,8 +2,6 @@ package com.konpi.quantummeteorology;
 
 import com.konpi.quantummeteorology.common.command.SeasonCommand;
 import com.konpi.quantummeteorology.common.command.TANCommand;
-import com.konpi.quantummeteorology.common.fluid.FluidBase;
-import com.konpi.quantummeteorology.common.init.ModConfig;
 import com.konpi.quantummeteorology.common.init.ModFertility;
 import com.konpi.quantummeteorology.common.init.ModHandlers;
 import com.konpi.quantummeteorology.common.init.ModStates;
@@ -22,13 +20,14 @@ import org.apache.logging.log4j.Logger;
  * 需要把各种init放进common proxy和client proxy里面去
  */
 @Mod(modid = Flower.MODID, name = Flower.NAME, version = Flower.VERSION, acceptedMinecraftVersions = "1.12.2")
-public class Flower {
+public class Flower
+{
 	public static final String MODID = "quantummeteorology";
 	public static final String NAME = "Quantum Meteorology";
 	public static final String VERSION = "1.0.1";
 	
 	public static Logger logger = LogManager.getLogger(Flower.NAME);
-
+	
 	@SidedProxy(clientSide = "com.konpi.quantummeteorology.proxy.ClientProxy", serverSide = "com.konpi.quantummeteorology.proxy.CommonProxy")
 	public static CommonProxy proxy;
 
@@ -39,26 +38,30 @@ public class Flower {
 	}
 
 	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
+	public void preInit(FMLPreInitializationEvent event)
+	{
 		ModStates.init();
 		proxy.preInit(event);
 	}
 
 	@Mod.EventHandler
-	public void init(FMLInitializationEvent event) {
+	public void init(FMLInitializationEvent event)
+	{
 		ModHandlers.init();
 		proxy.init(event);
 	}
 
 	@Mod.EventHandler
-	public void postInit(FMLPostInitializationEvent event) {
+	public void postInit(FMLPostInitializationEvent event)
+	{
 		ModFertility.init();
 		proxy.postInit(event);
 		ModHandlers.postInit();
 	}
 
 	@Mod.EventHandler
-	public void serverStarting(FMLServerStartingEvent event) {
+	public void serverStarting(FMLServerStartingEvent event)
+	{
 		event.registerServerCommand(new SeasonCommand());
 		event.registerServerCommand(new TANCommand());
 	}
