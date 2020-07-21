98328dfdbf7c4bf8bac8071291cc533fd8a2f3a5
konpi <chenangelo003@gmail.com>
2020-07-21 09:26:56 +0200

    gas

---
+++ src/main/java/com/konpi/quantummeteorology/api/capabilities/energy/IEnergy.java
@@ -0,0 +1,8 @@
+package com.konpi.quantummeteorology.api.capabilities.energy;
+
+public interface IEnergy {
+
+    public double getEnergyValuet();
+    // 设置电力
+    public void setEnergyValuet(double energyValuet);
+}
